426a6d8c0028fc1cefe7ec79b8695cb1587d1d23
zubaer <zubaer@stylinecollection.com>
2020-07-04 23:16:08 +0600

    added

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const express = require("express");
+var express_graphql = require('express-graphql');
+var { buildSchema } = require('graphql');
+var fs = require('fs');
+const app = express(); // create express app
+
+// GraphQL schema
+var schema = buildSchema(`
+    type Query {
+      message: String
+    },
+    type Mutation {
+      updateMessage(message: String!): String
+    }
+`);
+
+var getMessage = () => {
+  return fs.readFileSync("data.txt").toString('utf-8');
+} 
+
+var updateMessage = function({message}) {
+  fs.writeFileSync('data.txt', message, 'utf-8', function(err, data) {
+      if (err) throw err;
+  });
+  return fs.readFileSync("./data.txt").toString('utf-8');
+}
+
+var root = {  
+  message: getMessage,
+  updateMessage: updateMessage
+};
+
+// add middleware
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
+app.use('/graphql', express_graphql({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
+
+// start express server on port 5000
+app.listen(3000, () => {
+  console.log("server started on port 3000");
+});
--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import { gql } from "apollo-boost";
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import { ApolloProvider } from '@apollo/react-hooks';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const client = new ApolloClient({
+    uri: 'http://localhost:3000/graphql',
+  });
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>Twisker Assignment</h2>
+    </div>
+    <ExchangeRates></ExchangeRates>
+  </ApolloProvider>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const GET_MESSAGE = gql`
+  query fetchMessage {
+    message
+  }
+`;
+
+const CHANGE_MESSAGE = gql`
+    mutation updateMessage($message: String!) {
+        updateMessage(message: $message)
+    }
+`;
+
+function ExchangeRates() {
+    const [message, setMessage] = useState('')
+
+    const { loading, error, data } = useQuery(GET_MESSAGE);
+    const [updateMessage, { error2 }] = useMutation(CHANGE_MESSAGE, {
+        variables: { message }, refetchQueries: ["fetchMessage"]
+    });
+
+    if (error2) {
+        console.log('error: ', error)
+    }
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    return (
+        <div className="App">
+            <input
+                onChange={e => setMessage(e.target.value)}
+            />
+            <button onClick={updateMessage}>Update Message</button>
+            <div>
+                <strong>{data.message}</strong>
+            </div>            
+        </div>
+    );
+  }
+
+render(<App />, document.getElementById('root'));
