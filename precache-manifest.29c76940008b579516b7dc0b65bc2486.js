754e64c6055c182fca3c70b675887b42c38ef4f5
zubaer <zubaer@stylinecollection.com>
2020-07-06 15:03:59 +0600

    Updates

---
+++ precache-manifest.29c76940008b579516b7dc0b65bc2486.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4635f86fc4d40cc4beb923fa6c8105b6",
+    "url": "/react-apollo-graphql/index.html"
+  },
+  {
+    "revision": "2c61b3b33c450ffedcfc",
+    "url": "/react-apollo-graphql/static/css/main.e9a15d46.chunk.css"
+  },
+  {
+    "revision": "3bc16da9f5e264b28cef",
+    "url": "/react-apollo-graphql/static/js/2.553dc53f.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/react-apollo-graphql/static/js/2.553dc53f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c61b3b33c450ffedcfc",
+    "url": "/react-apollo-graphql/static/js/main.aa95b1bc.chunk.js"
+  },
+  {
+    "revision": "db599dd62c0c009b3b58",
+    "url": "/react-apollo-graphql/static/js/runtime-main.0cb667da.js"
+  }
+]);
